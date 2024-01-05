0803451e23b0d9432a0b40aa41be1d2cbf3ff5ce
kevin ly <kev3010@outlook.fr>
2024-01-05 21:08:35 +0100

    create post seeds

--- backend/database/seeders/20240105194525-Post.js
+++ backend/database/seeders/20240105194525-Post.js
@@ -3,15 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
     await queryInterface.bulkDelete('Posts', null, {});
     await queryInterface.bulkInsert(
       'Posts',
