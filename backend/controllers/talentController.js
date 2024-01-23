45bee2bd9d006a1bb7f5503b3b635e93fc0d79cc
kevin ly <kev3010@outlook.fr>
2024-01-23 23:13:13 +0100

    create talent update, delete, login, logout controller methods

--- backend/controllers/talentController.js
+++ backend/controllers/talentController.js
@@ -8,6 +8,7 @@ let db = require("../configs/postgresql"),
     sequelize = db.sequelize,
     Sequelize = db.Sequelize
 
+const talent = require("../database/models/talent")
 const talentService = require('../services/talentService')
 
 const talentController = {}
@@ -42,6 +43,50 @@ talentController.getById = async (req, res) => {
 }
 talentController.create = async (req, res) => {
     talentService.create(req.body)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(404).send({
+                message: err.message || 'Some error occurred while retrieving data.'
+            })
+        })
+}
+talentController.update = async (req, res) => {
+    talentService.update(req.params.id, req.body)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(404).send({
+                message: err.message || 'Some error occurred while retrieving data.'
+            })
+        })
+}
+talentController.delete = async (req, res) => {
+    talentService.delete(req.params.id)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(404).send({
+                message: err.message || 'Some error occurred while retrieving data.'
+            })
+        })
+}
+talentController.login = async (req, res) => {
+    talentService.login(req.body)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(404).send({
+                message: err.message || 'Some error occurred while retrieving data.'
+            })
+        })
+}
+talentController.logout = async (req, res) => {
+    talentService.logout(req.body)
         .then(data => {
             res.json(data)
         })
