73fb268d8d583af05c9ada76ebb6b2606d4dbded
kevin ly <kev3010@outlook.fr>
2024-01-30 23:54:34 +0100

    create login screen

---
+++ frontend/pf-inf/src/screens/loginPage.js
@@ -0,0 +1,17 @@
+function loginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+export default loginPage;
