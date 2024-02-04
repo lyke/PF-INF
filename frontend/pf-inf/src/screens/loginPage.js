a440468a187891eded4830fe08829682bba75a26
kevin ly <kev3010@outlook.fr>
2024-02-04 22:26:11 +0100

    fix css login page

--- frontend/pf-inf/src/screens/loginPage.js
+++ frontend/pf-inf/src/screens/loginPage.js
@@ -3,16 +3,18 @@ export default function LoginPage() {
   return (
     <div>
       <Navbar />
-      <h1>Login Page</h1>
-      <form>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" />
-        <br />
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" />
-        <br />
-        <button type="submit">Login</button>
-      </form>
+      <div className='login-container' >
+        <h1>Login Page</h1>
+        <form className='login-form' >
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" />
+          <br />
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
