fc43bd53512c52948f1054aebbcac2f6ae32ed1b
kevin ly <kev3010@outlook.fr>
2024-02-08 23:19:48 +0100

    function handleLogin

--- frontend/pf-inf/src/screens/loginPage.js
+++ frontend/pf-inf/src/screens/loginPage.js
@@ -1,5 +1,27 @@
 import Navbar from "../components/navbar";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 export default function LoginPage() {
+
+  // definir la fonction qui va gérer la connexion en vérifiant si le username et le password sont corrects
+  // si oui, rediriger vers la page d'accueil
+  // si non, afficher un message d'erreur
+  // const handleLogin = () => {
+  //   // récupérer les valeurs des inputs
+  //   const username = document.getElementById('username').value;
+  //   const password = document.getElementById('password').value;
+  //   // faire une requête POST à l'API pour vérifier si le username et le password sont corrects
+  //   axios.post('http://localhost:5000/api/login', {
+  //     username: username,
+  //     password: password
+  //   })
+  //     .then(response => {
+  //       console.log(response.data);
+  //       // si le username et le password sont corrects, rediriger vers la page d'accueil
+  //       // sinon, afficher un message d'erreur
+
+  //     });
+  }
   return (
     <div>
       <Navbar />
